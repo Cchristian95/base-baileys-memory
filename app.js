1b1d7ba60e80bad31c5241a3251931a449147354
Cchristian95 <calvachechristian@gmail.com>
2024-04-08 22:34:08 -0500

    ticket two

--- app.js
+++ app.js
@@ -4,38 +4,7 @@ const QRPortalWeb = require('@bot-whatsapp/portal')
 const BaileysProvider = require('@bot-whatsapp/provider/baileys')
 const MockAdapter = require('@bot-whatsapp/database/mock')
 
-
-
-const flowPrincipal = addKeyword(['hola', 'ole', 'alo','precio','valor','cuanto','que vale'])
-    .addAnswer('¡Hola! 😊 ¡Bienvenido a TicketYa ⭐! Estamos aquí para ayudarte en todo lo que necesites')
-    
-    .addAnswer('¡Tu satisfacción es nuestra prioridad,si el producto no cumple con tus expectativas te devolvemos tu dinero !')
-    
-    .addAnswer('Por favor indícame qué es lo que deseas hacer, Selecciona una opción (1, 2, o 3) y te ayudaré.')
-
-    .addAnswer(
-        [
-            '1. 🛍️ Ver Productos Disponibles.',
-            '2. 🚚 Envíos y Devoluciones.',
-            '3. 💬 Hablar con un Asesor.'            
-
-        ]
-    )
-const subFlujo1 = addKeyword(['1', 'uno', 'numero uno','numero 1','ver productos disponibles','productos disponibles'])
-    .addAnswer('Por favor, selecciona la opción que más te interese:')
-    
-    .addAnswer('Por favor indícame qué es lo que deseas hacer, Selecciona una opción (1, 2, o 3) o escribe en nombre del producto.')
-
-    .addAnswer(
-        [
-            '1. 👙 Kit glúteos.',
-            '2. 🧹 Trapero.',
-            '3. 🍽️ Escurridor.',            
-            '4. 💬 Hablar con un asesor.'            
-        ]
-    )
-
-    const contraEntrega = addKeyword(['pago contra entrega','contra entrega', 'contraentrega','contra reembolso','paga en casa'])
+const contraEntrega = addKeyword(['pago contra entrega', 'contra entrega', 'contraentrega', 'contra reembolso', 'paga en casa'])
     .addAnswer(
         [
 
@@ -58,7 +27,7 @@ const subFlujo1 = addKeyword(['1', 'uno', 'numero uno','numero 1','ver productos
         []
     )
 
-    const pagoEnLinea = addKeyword(['pago en linea','pagar online', 'pagar en tienda','pago anticipado','pagar en tienda en linea'])
+const pagoEnLinea = addKeyword(['pago en linea', 'pagar online', 'pagar en tienda', 'pago anticipado', 'pagar en tienda en linea'])
     .addAnswer(
         [
 
@@ -72,12 +41,12 @@ const subFlujo1 = addKeyword(['1', 'uno', 'numero uno','numero 1','ver productos
         []
     )
 
-    const hablarAsesor = addKeyword(['hablar con asesor', 'asesor', 'hablar con un asesor', 'hablar con alguien'])
+const hablarAsesor = addKeyword(['hablar con asesor', 'asesor', 'hablar con un asesor', 'hablar con alguien'])
     .addAnswer(
         [
             '​¡Hola ! 😊 ¡Mi nombre es Nathalia  🚀',])
     .addAnswer(
-        [    
+        [
             '📍 Queremos conocerte mejor:',
 
             '¿Cuál es tu nombre y en qué puedo ayudarte? 📦 🏙️',
@@ -86,11 +55,95 @@ const subFlujo1 = addKeyword(['1', 'uno', 'numero uno','numero 1','ver productos
         null,
         []
     )
+    
+    const kitSolar = addKeyword('a','kit solar','kit solar de emergencia')
+    .addAnswer(['⚡Kit Solar de Emergencia, a tan solo 💵 124.900💵',])
+    .addAnswer(['Envío gratuito y pago contra entrega. 🛒 a toda Colombia 🇨🇴',])
+    .addAnswer(['🔧 Características y Especificaciones: ',])
+    .addAnswer(
+        [
+            '3 Bombillos de 3W',
+            'Puerto USB 5V 0.5A',
+            'Radio FM con USB para MP3',
+            'Autonomía aproximada de hasta 8 horas',
+            'Tiempo de carga estimado con panel solar 12 a 15 horas',
+            'Tiempo de carga estimado con adaptador de corriente 8 a 10 horas',
+            'Dimensiones aproximadas del panel: 25.5 x 15 x 1.5 cm',
+        ],
+        null,
+        null,
+        []
+    )
+    .addAnswer(['Selecciona la opcion de pago',
+    'Puedes pagar en linea o tambien con pago contra entrega',] ,
+    null,
+    null,
+    []
+)
+.addAnswer(['Pago contra entrega',])
+.addAnswer(['Pago en linea'], null, null, [pagoEnLinea, contraEntrega])
+
+const subFlujo1 = addKeyword(['1', 'uno', 'numero uno', 'numero 1', 'Ver productos disponibles', 'ver productos disponibles','catalogo'])
+    .addAnswer('Por favor, selecciona la opción que más te interese:')
+
+    .addAnswer('Selecciona una opción (a, b, ...) o escribe en nombre del producto.')
+
+    .addAnswer(
+        [
+            'a. 🌞 Kit Solar de emergencia.',
+            'b. 🧹 Trapero triangular 360.',
+            'c. 🍽️ Escurridor de platos.',
+            'd. 📖 Ver catalogo.'
+        ],
+        null,
+        null,
+        [kitSolar]
+    )
+
+const subFlujo2 = addKeyword(['2', 'dos', 'numero dos', 'numero 2', 'Envíos y Devoluciones', 'envios y devoluciones'])
+.addAnswer('¡Entendido! ¿Qué información necesitas?')
+
+.addAnswer('Por favor indícame qué es lo que deseas hacer, Selecciona una opción (1, 2, o 3) o escribe en nombre del producto.')
+
+.addAnswer(
+    [
+        '1. 🚚 Envíos',
+        '2. 📦 Devoluciones',
+        '3. 💬Hablar con un asesor.'
+    ],
+    null,
+    null,
+    []
+)
+
+const flowPrincipal = addKeyword(['hola', 'hol','ola', 'ole', 'alo', 'precio', 'valor', 'cuanto', 'que vale'])
+    .addAnswer('¡Hola! 😊 ¡Bienvenido a TicketYa ⭐! Estamos aquí para ayudarte en todo lo que necesites')
+
+    .addAnswer('¡Tu satisfacción es nuestra prioridad,si el producto no cumple con tus expectativas te devolvemos tu dinero !')
+
+    .addAnswer('Por favor indícame qué es lo que deseas hacer, Selecciona una opción (1, 2, o 3) y te ayudaré.')
+
+    .addAnswer(
+        [
+            '1. 🛍️ Ver Productos Disponibles.',
+            '2. 🚚 Envíos y Devoluciones.',
+            '3. 💬 Hablar con un Asesor.'
+
+        ],
+        null,
+        null,
+        [subFlujo1,subFlujo2]
+    )
+const Gracias = addKeyword(['gracias','muchas gracias'])
+    .addAnswer('¡Te agradecemos por elegirnos! Esperamos tener la oportunidad de atenderte nuevamente. 🤝👊')
+    .addAnswer('¡Gracias por confiar en nosotros! 🌟🛍️')
+    .addAnswer('Visita nuestra tienda online.')
+    .addAnswer('https://spaceshopcolombia.com/')
 
 
 const main = async () => {
     const adapterDB = new MockAdapter()
-    const adapterFlow = createFlow([flowPrincipal, contraEntrega, pagoEnLinea, hablarAsesor, subFlujo1])
+    const adapterFlow = createFlow([flowPrincipal, contraEntrega, pagoEnLinea, hablarAsesor, Gracias])
     const adapterProvider = createProvider(BaileysProvider)
 
     createBot({
