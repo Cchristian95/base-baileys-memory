2f8b9aa033be7807d9e9e539c7dc380e301c803e
Cchristian95 <calvachechristian@gmail.com>
2024-04-09 08:48:32 -0500

    ticket 4

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ const QRPortalWeb = require('@bot-whatsapp/portal')
 const BaileysProvider = require('@bot-whatsapp/provider/baileys')
 const MockAdapter = require('@bot-whatsapp/database/mock')
 
-const contraEntrega = addKeyword(['pago contra entrega', 'contra entrega', 'contraentrega', 'contra reembolso', 'paga en casa'])
+const contraEntrega = addKeyword(['1','pago contra entrega', 'contra entrega', 'contraentrega', 'contra reembolso', 'paga en casa'])
+    
+    .addAnswer(['📦 ¡DATOS DE ENVÍO A CONTRA ENTREGA!',])
     .addAnswer(
-        [
-
-            '📦 ¡DATOS DE ENVÍO A CONTRA ENTREGA!',
-
+        [   
             'Nombre:',
             'Cédula:',
             'Ciudad:',
@@ -18,21 +17,31 @@ const contraEntrega = addKeyword(['pago contra entrega', 'contra entrega', 'cont
             'Barrio o referencia:',
             'Celular:',
             'Correo Electrónico:',
+        ])
+    .addAnswer(['Tus productos llegarán en un lapso de 2 a 6 días hábiles 📦 ',])
+    .addAnswer(['Envío gratuito y pago contra entrega. 🛒',])
+        
+const pagoEnLinea = addKeyword(['2','pago en linea', 'pagar online', 'pagar en tienda', 'pago anticipado', 'pagar en tienda en linea'])
+    .addAnswer(
+        [
 
-            'Características y especificaciones:👇',
+            '⭕Dale Clic al Enlace para Comprar en Linea👉🏻',
             'www.ticketya.com.co',
+
+            'Al Finalizar tu compra uno de nuestros asesores se pondra en contacto contigo para anexar la guia de su pedido.',
         ],
         null,
         null,
         []
     )
 
-const pagoEnLinea = addKeyword(['pago en linea', 'pagar online', 'pagar en tienda', 'pago anticipado', 'pagar en tienda en linea'])
+const verCatalogo = addKeyword(['4','ver catalogo', 'catalogo'])
     .addAnswer(
         [
 
-            '⭕Dale Clic al Enlace para Comprar en Linea👉🏻',
-            'www.ticketya.com.co',
+            '⭕Dale clic al enlace para ver nuestro catalogo en linea 👉🏻',
+            
+            'www.ticketya.com.co/tienda',
 
             'Al Finalizar tu compra uno de nuestros asesores se pondra en contacto contigo para anexar la guia de su pedido.',
         ],
@@ -56,7 +65,7 @@ const hablarAsesor = addKeyword(['hablar con asesor', 'asesor', 'hablar con un a
         []
     )
     
-    const kitSolar = addKeyword('a','kit solar','kit solar de emergencia')
+    const kitSolar = addKeyword('1','kit solar','kit solar de emergencia')
     .addAnswer(['⚡Kit Solar de Emergencia, a tan solo 💵 124.900💵',])
     .addAnswer(['Envío gratuito y pago contra entrega. 🛒 a toda Colombia 🇨🇴',])
     .addAnswer(['🔧 Características y Especificaciones: ',])
@@ -74,38 +83,96 @@ const hablarAsesor = addKeyword(['hablar con asesor', 'asesor', 'hablar con un a
         null,
         []
     )
-    .addAnswer(['Selecciona la opcion de pago',
-    'Puedes pagar en linea o tambien con pago contra entrega',] ,
-    null,
-    null,
-    []
-)
-.addAnswer(['Pago contra entrega',])
-.addAnswer(['Pago en linea'], null, null, [pagoEnLinea, contraEntrega])
+    .addAnswer(['Puedes pagar en linea o tambien con pago contra entrega',])
+    .addAnswer('Por favor, selecciona la opción de pago:')
+    .addAnswer(['1. Pago contra entrega',])
+    .addAnswer(['2. Pago en linea'], null, null, [pagoEnLinea, contraEntrega])
+
+    const traperoTriangular = addKeyword('2','trapero triangular','trapeador','trapero','trapero 360')
+    .addAnswer(['Trapero triangular 360, a tan solo 💵 79.900💵',])
+    .addAnswer(['Envío gratuito y pago contra entrega. 🛒 a toda Colombia 🇨🇴',])
+    .addAnswer(['🔧 Características y Especificaciones: ',])
+    .addAnswer(
+        [
+            '3 Bombillos de 3W',
+            'Puerto USB 5V 0.5A',
+            'Radio FM con USB para MP3',
+            'Autonomía aproximada de hasta 8 horas',
+            'Tiempo de carga estimado con panel solar 12 a 15 horas',
+            'Tiempo de carga estimado con adaptador de corriente 8 a 10 horas',
+            'Dimensiones aproximadas del panel: 25.5 x 15 x 1.5 cm',
+        ],
+        null,
+        null,
+        []
+    )
+    .addAnswer(['Puedes pagar en linea o tambien con pago contra entrega',])
+    .addAnswer('Por favor, selecciona la opción de pago:')
+    .addAnswer(['1. Pago contra entrega',])
+    .addAnswer(['2. Pago en linea'], null, null, [pagoEnLinea, contraEntrega])
+    
+    const escurridorPlatos = addKeyword('3','escurridor de platos','Escurridor de platos','escurridor platos')
+    .addAnswer(['🍽 Escurridor de platos, a tan solo 💵 119.900💵',])
+    .addAnswer(['Envío gratuito y pago contra entrega. 🛒 a toda Colombia 🇨🇴',])
+    .addAnswer(['🔧 Características y Especificaciones: ',])
+    .addAnswer(
+        [
+            '3 Bombillos de 3W',
+            'Puerto USB 5V 0.5A',
+            'Radio FM con USB para MP3',
+            'Autonomía aproximada de hasta 8 horas',
+            'Tiempo de carga estimado con panel solar 12 a 15 horas',
+            'Tiempo de carga estimado con adaptador de corriente 8 a 10 horas',
+            'Dimensiones aproximadas del panel: 25.5 x 15 x 1.5 cm',
+        ],
+        null,
+        null,
+        []
+    )
+    .addAnswer(['Puedes pagar en linea o tambien con pago contra entrega',])
+    .addAnswer('Por favor, selecciona la opción de pago:')
+    .addAnswer(['1. Pago contra entrega',])
+    .addAnswer(['2. Pago en linea'], null, null, [pagoEnLinea, contraEntrega])
 
 const subFlujo1 = addKeyword(['1', 'uno', 'numero uno', 'numero 1', 'Ver productos disponibles', 'ver productos disponibles','catalogo'])
     .addAnswer('Por favor, selecciona la opción que más te interese:')
 
-    .addAnswer('Selecciona una opción (a, b, ...) o escribe en nombre del producto.')
+    .addAnswer('Selecciona una opción (1, 2, ...) o escribe en nombre del producto.')
 
     .addAnswer(
         [
-            'a. 🌞 Kit Solar de emergencia.',
-            'b. 🧹 Trapero triangular 360.',
-            'c. 🍽️ Escurridor de platos.',
-            'd. 📖 Ver catalogo completo.'
+            '1. 🌞 Kit Solar de emergencia.',
+            '2. 🧹 Trapero triangular 360.',
+            '3. 🍽️ Escurridor de platos.',
+            '4. 📖 Ver catalogo completo.'
         ],
         null,
         null,
-        [kitSolar]
+        [kitSolar, traperoTriangular, escurridorPlatos, verCatalogo]
     )
 
+const envios = addKeyword(['1', 'uno', 'numero uno', 'numero 1', 'Envios', 'Envíos', 'pedido en transito','envio'])
+    .addAnswer('Escriba porfavor el numero de guia de su pedido')
+    .addAnswer(['En caso de no tener el numero de guia, seleccione la opcion *No tengo numero de guia*'])
+    .addAnswer(['1. *No tengo numero de guia*'],
+    null,
+    null,
+    [])
+
+const devoluciones = addKeyword(['2', 'dos', 'numero dos', 'numero 2', 'Devoluciones', 'devoluciones'])
+    .addAnswer('Escriba porfavor el numero de guia de su pedido, para validar su garantia')
+    .addAnswer(['En caso de no tener el numero de guia, seleccione la opcion *No tengo numero de guia*'])
+    .addAnswer(['1. *No tengo numero de guia*'],
+    null,
+    null,
+    [])
+
 const subFlujo2 = addKeyword(['2', 'dos', 'numero dos', 'numero 2', 'Envíos y Devoluciones', 'envios y devoluciones'])
-.addAnswer('¡Entendido! ¿Qué información necesitas?')
+    .addAnswer('¡Entendido! ¿Qué información necesitas?')
 
-.addAnswer('Por favor indícame qué es lo que deseas hacer, Selecciona una opción (1, 2, o 3) o escribe en nombre del producto.')
+    .addAnswer('Por favor indícame qué es lo que deseas hacer, Selecciona una opción (1, 2, o 3) o escribe en nombre del producto.')
 
-.addAnswer(
+    .addAnswer(
     [
         '1. 🚚 Envíos',
         '2. 📦 Devoluciones',
@@ -113,27 +180,35 @@ const subFlujo2 = addKeyword(['2', 'dos', 'numero dos', 'numero 2', 'Envíos y D
     ],
     null,
     null,
-    []
-)
+    [envios,devoluciones,hablarAsesor]
+    )
 
+const opcionesValidas = ['1', '2', '3'];
 const flowPrincipal = addKeyword(['hola', 'hol','ola', 'ole', 'alo', 'precio', 'valor', 'cuanto', 'que vale'])
     .addAnswer('¡Hola! 😊 ¡Bienvenido a TicketYa ⭐! Estamos aquí para ayudarte en todo lo que necesites')
 
     .addAnswer('¡Tu satisfacción es nuestra prioridad,si el producto no cumple con tus expectativas te devolvemos tu dinero !')
-
-    .addAnswer('Por favor indícame qué es lo que deseas hacer, Selecciona una opción (1, 2, o 3) y te ayudaré.')
-
+    
     .addAnswer(
         [
             '1. 🛍️ Ver Productos Disponibles.',
             '2. 🚚 Envíos y Devoluciones.',
             '3. 💬 Hablar con un Asesor.'
-
-        ],
-        null,
-        null,
-        [subFlujo1,subFlujo2]
+            
+        ]
     )
+    .addAnswer('Por favor seleccion tu tema de interesa, Escribe una opción (1, 2, o 3).',{capture:true},(ctx, {fallBack}) => {
+        
+        console.log("texto ingresado ", ctx.body)    
+            if (!opcionesValidas.includes(ctx.body)) {    
+                ctx.body = null
+                return fallBack();
+            }
+            console.log("texto de salida ", ctx.body)    
+            console.log("Opcion Valida")    
+        },[subFlujo1, subFlujo2, hablarAsesor])
+
+    
 const Gracias = addKeyword(['gracias','muchas gracias'])
     .addAnswer('¡Te agradecemos por elegirnos! Esperamos tener la oportunidad de atenderte nuevamente. 🤝👊')
     .addAnswer('¡Gracias por confiar en nosotros! 🌟🛍️')
